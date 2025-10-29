8b791fe8d47577056310680c5a70e59d232de9d4
LeanAranda <arandaleandro2003@gmail.com>
2025-10-28 22:59:05 -0300

    restored incremental id for events entity

--- rest-server/src/main/java/com/empujecomunitario/rest_server/entity/Event.java
+++ rest-server/src/main/java/com/empujecomunitario/rest_server/entity/Event.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 @Table(name = "events")
 public class Event {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
